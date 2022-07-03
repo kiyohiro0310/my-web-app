6a0748b59efd029c691a7b3419fd2284a53c7869
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2022-07-04 01:16:55 +0900

    Add docker file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   locales: ["en"],
-  defaultLocale: "en"
-}
+  defaultLocale: "en",
 
-module.exports = nextConfig
+  experimental: {
+    outputStandalone: true,
+  }
+}
